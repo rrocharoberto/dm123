9d2bd8127c3e446ad44aa6c47e89a32c7fdb5252
Roberto Rocha <rrocha.roberto@gmail.com>
2025-06-02 19:25:57 -0300

    Improve code, organize in functions

--- lambda-process-image/process_image_function.py
+++ lambda-process-image/process_image_function.py
@@ -11,53 +11,63 @@ table = dynamodb.Table('dm123-images')
 
 def lambda_handler(event, context):
     print("Iniciando processamento da imagem.")
-    # Evento de trigger 'S3 PUT'
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-        s3_url = f"s3://{bucket}/{key}"
+    print("event: " + json.dumps(event))
+    try:
+        # Evento de trigger 'S3 PUT'
+        s3_record = event['Records'][0]
+        print("s3_record: " + json.dumps(s3_record))
+        bucket_name = s3_record['s3']['bucket']['name']
+        object_key = s3_record['s3']['object']['key']
 
-        print(f"Processando bucket {bucket} objeto {key}")
-        try:
-            # Baixar imagem
-            response = s3.get_object(Bucket=bucket, Key=key)
-            img_data = response['Body'].read()
-            image = Image.open(BytesIO(img_data))
-            
-            # Extrair metadados básicos
-            metadata = {
-                "format": image.format,
-                "mode": image.mode,
-                "size": image.size,  # (width, height)
-            }
+        process_image(bucket_name, object_key)
+    except Exception as e:
+        print(f"Erro ao processar evento: {event}: {str(e)}")
+        raise
 
-            user_id = extract_user_id_from_event(event)
-            # Criar um registro para o DynamoDB
-            item = {
-                "imageId": str(uuid.uuid4()),
-                "userId": user_id,
-                "createdAt": datetime.utcnow().isoformat(),
-                "s3url": s3_url,
-                "status": "PROCESSADO",
-                "metadata": json.dumps(metadata)
-            }
-
-            # Inserir no DynamoDB
-            table.put_item(Item=item)
-
-        except Exception as e:
-            print(f"Erro ao processar {bucket}:{key}: {str(e)}")
-            raise
     print("Finalizando processamento da imagem.")
     return {
         'statusCode': 200,
         'body': 'Processamento concluído.'
     }
 
-def extract_user_id_from_event(event): # obtem os dados da credencial
+def process_image(bucket, key):
+    print(f"Processando bucket: {bucket} objeto: {key}")
+    try:
+        s3_url = f"s3://{bucket}/{key}"
+
+        # Baixar imagem
+        response = s3.get_object(Bucket=bucket, Key=key)
+        img_data = response['Body'].read()
+        image = Image.open(BytesIO(img_data))
+        
+        # Extrair metadados básicos
+        metadata = {
+            "format": image.format,
+            "mode": image.mode,
+            "size": image.size,  # (width, height)
+        }
+
+        user_id = get_user_id_from_s3_object(key)
+        # Criar um registro para o DynamoDB
+        item = {
+            "imageId": str(uuid.uuid4()),
+            "userId": user_id,
+            "createdAt": datetime.utcnow().isoformat(),
+            "s3url": s3_url,
+            "status": "PROCESSADO",
+            "metadata": json.dumps(metadata)
+        }
+
+        # Inserir no DynamoDB
+        table.put_item(Item=item)
+
+    except Exception as e:
+        print(f"Erro ao processar {bucket}:{key}: {str(e)}")
+        raise
+
+def get_user_id_from_s3_object(object_key):
   try:
-    # 'username': o nome de usuário visível no Cognito, se preferir algo mais legível.
-    # 'sub': um identificador único do usuário no Cognito.
-    return event['context']['authorizer']['claims']['username']
+    # Extracts the first part of an S3 object key (before the first slash).
+    return object_key.split('/')[0]
   except KeyError:
     return "desconhecido"
