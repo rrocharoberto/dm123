b1259a7dcfb75dd17677cd98f005c7ffb8c6a86d
Roberto Rocha <rrocha.roberto@gmail.com>
2025-06-04 07:53:17 -0300

    Add notify slack lambda

---
+++ lambda-notify-team/notify_team_function.py
@@ -0,0 +1,27 @@
+import urllib3 
+import json
+import os
+
+http = urllib3.PoolManager() 
+
+def lambda_handler(event, context): 
+    print("Running slack lambda: " + str(event))
+
+    message = event['Records'][0]['Sns']['Message']
+    parsed_message = json.loads(message)
+
+    url = os.environ["SLACK_HOOK_URL"]
+    msg = {
+        "channel": os.environ["SLACK_CHANNEL"],
+        "username": "aws-alarm",
+        "text": "\nAlarm: " + parsed_message['AlarmName']
+        + "\nState: " + parsed_message['NewStateValue']
+        + "\nReason: " + parsed_message['NewStateReason']
+    }
+    encoded_msg = json.dumps(msg).encode('utf-8')
+    resp = http.request('POST', url, body=encoded_msg)
+    print({
+        "message": event['Records'][0]['Sns']['Message'], 
+        "status_code": resp.status, 
+        "response": resp.data
+    })
